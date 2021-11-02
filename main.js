b481f7e6179d193690af4e3576b85397486e8ac5
Zyon <zyonamadeus01@gmail.com>
2021-11-02 20:06:19 -0300

    Exercicio 1

---
+++ main.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { DOMParser } = require('xmldom');
+const R = require('ramda');
+
+const { isValid, elementsToArray, getGitHubProject } = require('./xmlfilter');
+
+// efetua o processamento do xml e armazena a estrutura no objeto 'document'
+const document = new DOMParser().parseFromString(fs.readFileSync('res/f-droid.xml', 'utf-8'));
+
+const isAddedAfter2018AndUpdatedAfter2019 = isValid(R.__, 2018, 2019);
+
+const addedApps = elementsToArray(document.getElementsByTagName('application'))
+    .filter(isAddedAfter2018AndUpdatedAfter2019)
+    .map(getGitHubProject);
+
+console.log(addedApps.join('\n'));
+ 
+// (Exercício 1) Identifique todas as declarações de funções neste projeto
+    // Adicione um comentário identificando-as
+
+// contentOfTag, linha 3
+// getGithubProject, linha 11
+// elementsToArray, linha 13
+// isValid, linha 20
